4c794cc3fc0e0f13fc4fdfbbd305fed423bf3a38
Gustavo Maciel Pires <gustavomp.ti@gmail.com>
2017-05-07 12:22:20 -0300

    corrigido

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,27 +1,37 @@
-angular.module('starter.controllers', [])
+angular.module('MapController', ['ngCordova'])
+    .controller('MapController', function($scope, $state,$cordovaGeolocation,$ionicLoading,$ionicPlatform){
 
-.controller('MapCtrl', function($scope, $ionicLoading) {
-  $scope.mapCreated = function(map) {
-    $scope.map = map;
-  };
+     $ionicPlatform.ready(function() {    
+ 
+        
+         
+        var posOptions = {
+            enableHighAccuracy: true,
+            timeout: 20000,
+            maximumAge: 0
+        };
+ 
+        $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+            var lat  = position.coords.latitude;
+            var long = position.coords.longitude;
+             
+            var myLatlng = new google.maps.LatLng(lat, long);
+             
+            var mapOptions = {
+                center: myLatlng,
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };          
+             
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);          
+             
+            $scope.map = map;   
+            $ionicLoading.hide();           
+             
+        }, function(err) {
+            $ionicLoading.hide();
+            console.log(err);
+        });
+    })               
 
-  $scope.centerOnMe = function () {
-    console.log("Centering");
-    if (!$scope.map) {
-      return;
-    }
-
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-
-    navigator.geolocation.getCurrentPosition(function (pos) {
-      console.log('Got pos', pos);
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
-    }, function (error) {
-      alert('Unable to get location: ' + error.message);
-    });
-  };
-});
+  });
